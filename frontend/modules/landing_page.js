d29b5b2619660d547ecb9c5aedcc1745408e53c6
CrioUser <crio-user@criodo.com>
2023-03-19 14:03:33 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -2,8 +2,9 @@ import config from "../conf/index.js";
 
 async function init() {
   //Fetches list of all cities along with their images and description
-  let cities = await fetchCities();
 
+  let cities = await fetchCities();
+  console.log(cities);
   //Updates the DOM with the cities
   if (cities) {
     cities.forEach((key) => {
@@ -11,19 +12,21 @@ async function init() {
     });
   }
 }
-
+CanvasCaptureMediaStreamTrack;
 //Implementation of fetch call
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try {
+    const response = await fetch("http://65.1.216.226:8082/cities");
+    return response.json();
+  } catch (err) {alert(err);}
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-
 }
 
 export { init, fetchCities, addCityToDOM };
